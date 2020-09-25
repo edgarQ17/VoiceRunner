550d6cd53f430d5822e43f6eb86426d16d77760a
rubenvecg <rubenvec95@gmail.com>
2020-09-25 16:47:01 -0400

    Main loop

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public GameObject box;
+    private float firstX = 0.0f;
+    private float floorLevel = -3.17f;
+
+    private float frequency = 1.0f;
+    private float timer;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(box, new Vector2(firstX + 20, floorLevel), Quaternion.identity);
+        timer = frequency;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if(timer <= 0.0f)
+        {
+            Instantiate(box, new Vector2(firstX + 20, floorLevel), Quaternion.identity);
+            timer = frequency;
+        }
+
+        
+    }
+}
---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float backwardsSpeed = -0.1f;
+    private Camera cam;
+
+    void Start()
+    {
+        cam = FindObjectOfType<Camera>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + new Vector3(backwardsSpeed, 0.0f, 0.0f);
+
+        Vector3 viewPos = cam.WorldToViewportPoint(transform.position);
+        
+
+        if (viewPos.x < -0.1f) {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
