203a11780344019645427006861cde87be481e5f
Edgar Quintero <omarquintero092@gmail.com>
2020-09-25 22:50:13 -0400

    new commit

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,24 +7,44 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     private float backwardsSpeed = -0.1f;
     private Camera cam;
+///////player movement
+ public float Speed;    ///player speed
+ public Rigidbody2D rigidb;
 
-    void Start()
-    {
+ public float jumpForce = 20f;///default jjump
+
+ float hx;
+ 
+    void Start(){
         cam = FindObjectOfType<Camera>();
-        
     }
 
     // Update is called once per frame
+     
     void Update()
     {
+        hx = Input.GetAxisRaw("Horizontal");
+
+     if(Input.GetButtonDown("Jump") ){            //////this will check if grounded to allow jump
+         Jump(); ///jump mthod
+     }
+
         transform.position = transform.position + new Vector3(backwardsSpeed, 0.0f, 0.0f);
 
         Vector3 viewPos = cam.WorldToViewportPoint(transform.position);
         
-
-        if (viewPos.x < -0.1f) {
-            Destroy(this.gameObject);
-        }
+      //  if (viewPos.x < -0.1f) {
+         //   Destroy(this.gameObject);
+       /// }
         
     }
+    private void moveUpdate() {   ////player move method
+     Vector2 movement = new Vector2(hx * Speed, rigidb.velocity.y );
+     rigidb.velocity = movement;
 }
+    void Jump(){
+        Vector2 movement = new Vector2(rigidb.velocity.x, jumpForce);
+        rigidb.velocity = movement;
+    }
+}
+
